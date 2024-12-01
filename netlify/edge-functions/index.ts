fcb290ec5adf334f3780ed48f0c92f0554ba9dab
aungchanpyae3 <pyae62436@gmail.com>
2024-12-01 17:29:59 +0630

    init

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -23,11 +23,6 @@ export default async (request: Request) => {
     // Create new headers, extend original headers, and set caching headers
     const headers = new Headers(fetchData.headers);
     headers.set("Access-Control-Allow-Origin", "*");
-    headers.set("Cache-Control", "public, max-age=31536000, s-maxage=31536000");
-    headers.set(
-      "Netlify-CDN-Cache-Control",
-      "public, max-age=31536000, s-maxage=31536000"
-    );
 
     // Return response with binary data
     return new Response(body, {
